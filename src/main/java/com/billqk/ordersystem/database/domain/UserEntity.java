812897abf1cd73045478810426f11fcd8fa9dcb3
bill <billngo47@gmail.com>
2021-08-05 00:37:39 +0700

    add variable and primary key

--- src/main/java/com/billqk/ordersystem/database/domain/UserEntity.java
+++ src/main/java/com/billqk/ordersystem/database/domain/UserEntity.java
@@ -1,2 +1,28 @@
-package com.billqk.ordersystem.database.domain;public class UserEntity {
+package com.billqk.ordersystem.database.domain;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.net.URL;
+
+@Entity
+@Table(name = "/user")
+@EntityListeners(AuditingEntityListener.class)
+public class UserEntity {
+    // Primary Key
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", updatable = false, nullable = false)
+    private int id;
+
+    private String firstname;
+    private String lastname;
+    private URL email;
+    private int age;
+
+
+
+
 }
