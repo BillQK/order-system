469e50e3b1b2096c508f398042bc78b8c28b8c8e
nqngo@ucdavis.edu <nqngo@ucdavis.edu>
2021-08-12 23:39:30 -0700

    Added test infrastructure and h2 database

---
+++ src/test/java/com/billqk/ordersystem/OrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billqk.ordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
