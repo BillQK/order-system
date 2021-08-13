093c3937dbea809a2fdeec2ca054443e7fa388c1
BillQK <billngo47@gmail.com>
2021-08-13 17:06:00 +0700

    Added test for api create user and order

---
+++ src/test/java/com/billqk/ordersystem/OrderControllerTest.java
@@ -0,0 +1,69 @@
+package com.billqk.ordersystem;
+
+import com.billqk.ordersystem.controller.OrderController;
+import com.billqk.ordersystem.database.domain.OrderEntity;
+import com.billqk.ordersystem.database.domain.UserEntity;
+import com.billqk.ordersystem.database.repository.OrderRepository;
+import com.billqk.ordersystem.model.OrderDto;
+import com.billqk.ordersystem.model.UserDto;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class OrderControllerTest extends AbstractTest {
+        @Override
+        @Before
+        public void setUp() {
+            super.setUp();
+        }
+
+        @Test
+        public void CreateUser() throws Exception {
+            String uri = "/api/user/";
+            UserDto userDto = new UserDto();
+            userDto.setAge(19);
+            userDto.setEmail("123@gmail.com");
+            userDto.setFirstname("hi");
+            userDto.setLastname("hello");
+
+            String inputJson = super.mapToJson(userDto);
+            MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
+
+            int status = mvcResult.getResponse().getStatus();
+            assertEquals(201, status);
+            String content = mvcResult.getResponse().getContentAsString();
+            assertEquals(content, "User added");
+
+        }
+
+        @Test
+        public void createOrder() throws Exception {
+            String uri = "/api/order/";
+
+
+            OrderDto orderDto = new OrderDto();
+            orderDto.setOrderId(Long.valueOf(1));
+            orderDto.setUserId(Long.valueOf(1));
+            orderDto.setStatus("IN PROGRESS");
+
+            String inputJson = super.mapToJson(orderDto);
+            MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
+
+            int status = mvcResult.getResponse().getStatus();
+            assertEquals(201, status);
+            String content = mvcResult.getResponse().getContentAsString();
+            assertEquals(content, "Order added");
+    }
+
+}
