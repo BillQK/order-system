163a53232fab979ed45b22cefe983297123101fd
BillQK <billngo47@gmail.com>
2021-08-17 12:37:37 +0700

    Implement basic http security

--- src/main/java/com/billqk/ordersystem/security/SecurityConfiguration.java
+++ src/main/java/com/billqk/ordersystem/security/SecurityConfiguration.java
@@ -1,4 +1,38 @@
 package com.billqk.ordersystem.security;
 
-public class SecurityConfiguration {
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable();
+
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST).authenticated();
+
+        http
+                .httpBasic();
+
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}pass")
+                .roles("admin");
+    }
+
 }
