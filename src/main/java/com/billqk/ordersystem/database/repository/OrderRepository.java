66ae8e249ec539358a56357959674b0cfdd26f45
BillQK <billngo47@gmail.com>
2021-08-25 15:43:48 +0700

    Get order by status with desc date

--- src/main/java/com/billqk/ordersystem/database/repository/OrderRepository.java
+++ src/main/java/com/billqk/ordersystem/database/repository/OrderRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
-    List<OrderEntity> findByStatus (Constant.Status status);
+    List<OrderEntity> findByStatusOrderByOrderDateDesc(Constant.Status status);
 }
