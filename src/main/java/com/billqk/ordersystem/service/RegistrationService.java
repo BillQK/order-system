e7c946a6ca8caed41881fe016e5c771ba44ae134
BillQK <billngo47@gmail.com>
2021-08-28 19:05:09 +0700

    update admin registration

--- src/main/java/com/billqk/ordersystem/service/RegistrationService.java
+++ src/main/java/com/billqk/ordersystem/service/RegistrationService.java
@@ -4,7 +4,6 @@ import com.billqk.ordersystem.constant.Constant;
 import com.billqk.ordersystem.database.domain.UserEntity;
 import com.billqk.ordersystem.service.token.ConfirmationToken;
 import com.billqk.ordersystem.service.token.ConfirmationTokenService;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -35,25 +34,39 @@ public class RegistrationService {
                         request.getEmail(),
                         request.getPassword(),
                         Constant.Roles.USER
+                )
+        );
+    }
+
+    public String registerAdmin(RegistrationRequest request) {
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalStateException("email not valid");
+        }
+        return myUserService.signUpUser(
+                new UserEntity(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        Constant.Roles.ADMIN
 
                 )
         );
     }
 
     @Transactional
-    public String confirmToken(String token)
-    {
+    public String confirmToken(String token) {
         ConfirmationToken confirmationToken = confirmationTokenService.getToken(token)
                 .orElseThrow(
                         () -> new IllegalStateException("Token Not Found")
                 );
-        if (confirmationToken.getConfirmedAt() != null)
-        {
+        if (confirmationToken.getConfirmedAt() != null) {
             throw new IllegalStateException("Email Already confirmed");
 
         }
         LocalDateTime expiredAt = confirmationToken.getExpiredAt();
-        if(expiredAt.isBefore(LocalDateTime.now())){
+        if (expiredAt.isBefore(LocalDateTime.now())) {
             throw new IllegalStateException("Token Expired");
         }
 
@@ -63,4 +76,6 @@ public class RegistrationService {
         );
         return "Confirmed";
     }
+
+
 }
