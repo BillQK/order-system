4f352910e4eb69305af443131fe42855decda7a7
BillQK <billngo47@gmail.com>
2022-10-07 12:26:24 -0400

    push :

--- src/main/java/com/billqk/ordersystem/service/EmailService.java
+++ src/main/java/com/billqk/ordersystem/service/EmailService.java
@@ -4,8 +4,6 @@ package com.billqk.ordersystem.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -17,27 +15,27 @@ import javax.mail.internet.MimeMessage;
 @Service
 public class EmailService implements EmailSender {
 
-    Logger LOGGER = LoggerFactory
-            .getLogger(EmailService.class);
+  Logger LOGGER = LoggerFactory
+          .getLogger(EmailService.class);
 
-    @Autowired
-    private JavaMailSender mailSender;
+  @Autowired
+  private JavaMailSender mailSender;
 
-    @Override
-    @Async
-    public void send(String to, String email) {
-        try {
-            MimeMessage mimeMessage = mailSender.createMimeMessage();
-            MimeMessageHelper helper
-                    = new MimeMessageHelper(mimeMessage, "utf-8");
-            helper.setText(email, true);
-            helper.setTo(to);
-            helper.setSubject("Confirm your email");
-            helper.setFrom("Hello@amigoscode.com");
-            mailSender.send(mimeMessage);
-        } catch (MessagingException e) {
-            LOGGER.error("Failed to send email", e);
-            throw new IllegalStateException("failed to send email");
-        }
+  @Override
+  @Async
+  public void send(String to, String email) {
+    try {
+      MimeMessage mimeMessage = mailSender.createMimeMessage();
+      MimeMessageHelper helper
+              = new MimeMessageHelper(mimeMessage, "utf-8");
+      helper.setText(email, true);
+      helper.setTo(to);
+      helper.setSubject("Confirm your email");
+      helper.setFrom("Hello@gmail.com");
+      mailSender.send(mimeMessage);
+    } catch (MessagingException e) {
+      LOGGER.error("Failed to send email", e);
+      throw new IllegalStateException("failed to send email");
     }
+  }
 }
